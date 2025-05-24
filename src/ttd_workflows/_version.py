ea6dd5e0ebfcc8d7f6ccca297c56fa26d05d5f06
yali yuan <yali.yuan@thetradedesk.com>
2025-05-23 19:18:57 -0600

    choose versino

--- src/ttd_workflows/_version.py
+++ src/ttd_workflows/_version.py
@@ -3,10 +3,10 @@
 import importlib.metadata
 
 __title__: str = "ttd-workflows"
-__version__: str = "0.5.2"
+__version__: str = "0.4.2"
 __openapi_doc_version__: str = "v1"
 __gen_version__: str = "2.610.0"
-__user_agent__: str = "speakeasy-sdk/python 0.5.2 2.610.0 v1 ttd-workflows"
+__user_agent__: str = "speakeasy-sdk/python 0.4.2 2.610.0 v1 ttd-workflows"
 
 try:
     if __package__ is not None:
