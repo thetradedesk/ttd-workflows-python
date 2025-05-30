4fd6530c1ea88f40a7bb0ce41457fb4b900b6c46
yali yuan <yali.yuan@thetradedesk.com>
2025-05-30 13:58:35 -0600

    fix version

--- src/ttd_workflows/_version.py
+++ src/ttd_workflows/_version.py
@@ -3,10 +3,10 @@
 import importlib.metadata
 
 __title__: str = "ttd-workflows"
-__version__: str = "0.5.1"
+__version__: str = "0.5.0"
 __openapi_doc_version__: str = "v1"
 __gen_version__: str = "2.616.1"
-__user_agent__: str = "speakeasy-sdk/python 0.5.1 2.616.1 v1 ttd-workflows"
+__user_agent__: str = "speakeasy-sdk/python 0.5.0 2.616.1 v1 ttd-workflows"
 
 try:
     if __package__ is not None:
